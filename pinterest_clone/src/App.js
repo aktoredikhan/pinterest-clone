15c8b45c8792963e025887f1ebb395862c1f76c6
aktoredikhan <148527054+aktoredikhan@users.noreply.github.com>
2023-12-20 00:23:26 +0600

    Add files via upload

---
+++ pinterest_clone/src/App.js
@@ -0,0 +1,10 @@
+import Final from "./components/final/final";
+
+function App() {
+  
+  return (
+    <Final />
+  )
+}
+
+export default App;
---
+++ pinterest_clone/src/components/final/final.js
@@ -0,0 +1,67 @@
+import React, { Component, useState } from "react";
+import './final.css';
+import Pin from "../pin/pin";
+import Modal from "../modal/modal";
+
+class Final extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pins: [],
+            show_modal: false
+        }
+    }
+
+    add_pin = pinDetails => {
+        this.setState(_state => {
+            const new_pins = [
+                ..._state.pins
+            ]
+
+            new_pins.push(
+                <Pin pinDetails={pinDetails} key={_state.pins.length} />
+            )
+            return {
+                pins: new_pins,
+                show_modal: false
+            }
+
+
+        });
+    }
+    render() {
+        return (
+            <div>
+                  <div className="text1">
+                        <h1>
+                            Post your beautiful photos
+                        </h1>
+                    </div>
+                <div className="navigation_bar">
+                    
+
+                    <div onClick={() => this.setState({ show_modal: true })} className="pint_mock_icon_container add_pin">
+
+                        <img src="./images/add.png" alt="add_pin" className="pint_mock_icon" />
+                    </div>
+                </div>
+
+                <div className="pin_container">
+                    {this.state.pins}
+                </div>
+
+                <div onClick={event => event.target.className === 'add_pin_modal' ? this.setState({ show_modal: false }) : null}
+                    className="add_pin_modal_container"
+                >
+                    {
+                        this.state.show_modal ?
+                            <Modal add_pin={this.add_pin} /> : null
+                    } </div>
+            </div>
+        )
+    }
+}
+
+export default Final;
